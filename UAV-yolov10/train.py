066e09fa6b983768e8b9f54ac89435dbf3dacda7
MengmingWu <136443281+MengmingWu@users.noreply.github.com>
2025-01-06 20:14:54 +0800

    Delete UAV-yolov10 directory

--- UAV-yolov10/train.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLOv10
-model = YOLOv10(r'D:\yolov10-main\ultralytics\cfg\models\v10\Faster_DRB_SE.yaml')  # build a new model from YAML
-if __name__=="__main__":
-    model.train(data='datasets\Raruco\data.yaml', epochs=250 , name="train", batch=2, close_mosaic=0)
-  
